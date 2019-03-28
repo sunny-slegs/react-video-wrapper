569868d1d2f30a36ca6430ecec97b5f7252184fd
Sonya Slegers <sonjay08@gmail.com>
2019-03-27 19:11:01 -0500

    added Header and Player components

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function Header(props) {
+    return (
+      <div className="Header" role="banner">
+        <h1>React Player</h1>
+      </div>
+    );
+  }
+
+
+export default Header;
