664981e33b08b611dce24366e7e8fba539b49a3d
Sonya Slegers <sonjay08@gmail.com>
2019-03-28 00:17:53 -0500

    added mute button, added closed captions but captions are not showing up

--- src/Player.js
+++ src/Player.js
@@ -8,6 +8,8 @@ class Player extends Component {
       playing: false,
       playbackRate: 1,
       volume: .5,
+      mute: false,
+      subtitles: false,
     }
 
   }
@@ -47,7 +49,7 @@ class Player extends Component {
     const incVolume = () => {
       console.log(this.state.volume)
       if (this.state.volume >= .9) {
-        console.log("You cannot increase the volume, be kind to your ears!")
+        console.log("The volume can't go any higher")
       } else {
         const newVolume = this.state.volume + .1;
         this.setState({
@@ -66,11 +68,44 @@ class Player extends Component {
         })
       }
     }
+
+    const mute = () => {
+      const muteStatus =  this.state.mute;
+      this.setState({
+        mute: !muteStatus
+      })
+    }
   
+    if (this.state.mute === false) {
+      this.muteButton = "Mute"
+    } else {
+      this.muteButton = "Unmute"
+    }
+
+    const subtitles = () => {
+      const subtitleState = this.state.subtitles;
+      this.setState({
+        subtitles: !subtitleState
+      })
+    }
+
+    if (this.state.subtitles === false) {
+        this.config = {file: {}}
+        this.subtitleButton = "Show Closed Captions"
+    } else {
+       this.config = {file: {
+          tracks: [
+            {kind: 'captions', src: '/Users/sonyaslegers/Desktop/touch-to/src/audio-eng.vtt', srcLang: "en", default: true}
+          ]
+        }};
+        this.subtitleButton = "Hide Closed Captions"
+    };
+    
     return (
         <React.Fragment>
           <ReactPlayer url='http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' playing={this.state.playing}  
-          playbackRate={this.state.playbackRate} volume={this.state.volume}/>
+          playbackRate={this.state.playbackRate} volume={this.state.volume} muted={this.state.mute}
+          config={this.config}/>
           <button onClick={play}>Play</button>
           <button onClick={pause}>Pause</button>
           <button onClick={incPlaybackRate}>Increase Playback Rate</button>
@@ -78,10 +113,13 @@ class Player extends Component {
           <button onClick={incVolume}>+</button>
           <span>Volume</span>
           <button onClick={decVolume}>-</button>
+          <button onClick={mute}>{this.muteButton}</button>
+          <button onClick={subtitles}>{this.subtitleButton}</button>
         </React.Fragment>
       )
     }
   }
+  
 
 
 export default Player;
