868d48609fdd217cd0aba7a159465abcf05044cb
Sonya Slegers <sonjay08@gmail.com>
2019-03-28 00:50:19 -0500

    fixed caption button typo

--- src/Player.js
+++ src/Player.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactPlayer from 'react-player';
+import { Slider, Direction } from 'react-player-controls'
 
 class Player extends Component {
   constructor(props) {
@@ -14,83 +15,90 @@ class Player extends Component {
 
   }
   
-  render() {
+  play = () => {
+    this.setState ({
+      playing: true
 
-    const play = () => {
-      this.setState ({
-        playing: true
+    })
   
-      })
-    
-    }
-    const pause = () => {
-      this.setState({
-        playing: false
-      })
-    }
-
-    const incPlaybackRate = () => {
-      const newRate = this.state.playbackRate + 1;
+  }
+  pause = () => {
+    this.setState({
+      playing: false
+    })
+  }
+  
+  // Decrease playback rate
+  incPlaybackRate = () => {
+    const newRate = this.state.playbackRate + 1;
+    this.setState({
+      playbackRate: newRate
+    });
+  }
+  // Increase playback rate
+  decPlaybackRate = () => {
+    if (this.state.playbackRate <= 1) {
+      console.log("You cannot set a slower speed")
+    } else {
+      const newRate = this.state.playbackRate - 1;
       this.setState({
         playbackRate: newRate
       });
     }
-    const decPlaybackRate = () => {
-      if (this.state.playbackRate <= 1) {
-        console.log("You cannot set a slower speed")
-      } else {
-        const newRate = this.state.playbackRate - 1;
-        this.setState({
-          playbackRate: newRate
-        });
-      }
-    }
-
-    const incVolume = () => {
-      if (this.state.volume >= .9) {
-        console.log("The volume can't go any higher")
-      } else {
-        const newVolume = this.state.volume + .1;
-        this.setState({
-          volume: newVolume
-        })
-      }
-    }
-
-    const decVolume = () => {
-      if (this.state.volume <= .1 ) {
-        console.log("The volume can't go any lower") 
-      } else {
-        const newVolume = this.state.volume - .1;
-        this.setState({ 
-          volume: newVolume
-        })
-      }
-    }
-// toggle mute state
-    const mute = () => {
-      const muteStatus =  this.state.mute;
+  }
+  // Increase volume
+  incVolume = () => {
+    if (this.state.volume >= .9) {
+      console.log("The volume can't go any higher")
+    } else {
+      const newVolume = this.state.volume + .1;
       this.setState({
-        mute: !muteStatus
+        volume: newVolume
       })
     }
+  }
+    
+// Decrease volume
+  decVolume = () => {
+    if (this.state.volume <= .1 ) {
+      console.log("The volume can't go any lower") 
+    } else {
+      const newVolume = this.state.volume - .1;
+      this.setState({ 
+        volume: newVolume
+      })
+    }
+  }
+  // Toggle mute state
+  mute = () => {
+    const muteStatus =  this.state.mute;
+    this.setState({
+      mute: !muteStatus
+    })
+  }
+// Toggle captions state
+    captions = () => {
+      const captionState = this.state.captions;
+      this.setState({
+        captions: !captionState
+      })
+    }
+// Jump back 10 seconds
+onDuration
+// Jump forward 10 seconds
+// Seek within the video and see the progress
+// Show the seconds elapsed and the duration
+  render() {
   // Toggle mute/unmute button
     if (this.state.mute === false) {
       this.muteButton = "Mute"
     } else {
       this.muteButton = "Unmute"
     }
-// Toggle captions state
-    const captions = () => {
-      const captionState = this.state.captions;
-      this.setState({
-        captions: !captionState
-      })
-    }
 //Toggle caption show/hide button and config setting
     if (this.state.captions === false) {
         this.config = {file: {}}
-        this.captionsButton = "Show Closed Captions"
+        this.captionButton = "Show Closed Captions"
     } else {
        this.config = {file: {
           tracks: [
@@ -105,15 +113,15 @@ class Player extends Component {
           <ReactPlayer url='http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4' playing={this.state.playing}  
           playbackRate={this.state.playbackRate} volume={this.state.volume} muted={this.state.mute}
           config={this.config}/>
-          <button onClick={play}>Play</button>
-          <button onClick={pause}>Pause</button>
-          <button onClick={incPlaybackRate}>Increase Playback Rate</button>
-          <button onClick={decPlaybackRate}>Decrease Playback Rate</button>
-          <button onClick={incVolume}>+</button>
+          <button onClick={this.play}>Play</button>
+          <button onClick={this.pause}>Pause</button>
+          <button onClick={this.incPlaybackRate}>Increase Playback Rate</button>
+          <button onClick={this.decPlaybackRate}>Decrease Playback Rate</button>
+          <button onClick={this.incVolume}>+</button>
           <span>Volume</span>
-          <button onClick={decVolume}>-</button>
-          <button onClick={mute}>{this.muteButton}</button>
-          <button onClick={captions}>{this.captionButton}</button>
+          <button onClick={this.decVolume}>-</button>
+          <button onClick={this.mute}>{this.muteButton}</button>
+          <button onClick={this.captions}>{this.captionButton}</button>
         </React.Fragment>
       )
     }
